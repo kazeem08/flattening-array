d69c92a34772f7a1c0036b142df8e9604727539a
Kjimoh <kjimoh@terragonltd.com>
2022-01-24 22:57:03 +0100

    added more comments

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-module.exports = {
-    flatten(input){
+    /**
+     * 
+     * @param {*} input 
+     * @returns []
+     */
+    
+    function flatten(input){
+
+        // validate input type
         if(!Array.isArray(input)){
             return 'input should be of type array';
         }
+
+        const flattenedArray = input.reduce((previousElement, currentElement) => {
+
+            // check type of current element
+            if (Array.isArray(currentElement)) {
+
+                // use recursion on the array
+                previousElement = previousElement.concat(flatten(currentElement));
+            } else {
+                // push element into arrrayHolder
+                previousElement.push(currentElement);
+            }
+
+            return previousElement;
+
+        }, []); // set initial value
+
+        return flattenedArray;
     }
-}
+
+    module.exports = flatten;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,13 @@
-const { flatten } = require('./index');
+const flatten = require('./index');
 
-test("Returns invalid input if type passed is not array", () => {
+test("Returns invalid input if type passed is an obect", () => {
     expect(flatten({ array: [1,2,4,4]})).toBe('input should be of type array');
+});
+
+test("Returns invalid input if type passed is a string", () => {
+    expect(flatten('1,2,3,4,5')).toBe('input should be of type array');
+});
+
+test("Returns flattened array", () => {
+    expect(flatten([1, [2, [3], 4, [5, 6, [7]]]])).toEqual([1,2,3,4,5,6,7]);
 });
